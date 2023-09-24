5ddf6bb1ba194073e30b0268a3f13d5e2f640f81
devsince2021 <dev.ryu2021@gmail.com>
2023-09-25 00:57:33 +0900

    Feat: Post 상세 api 작성 (#11)

--- src/main/java/com/han/controller/PostController.java
+++ src/main/java/com/han/controller/PostController.java
@@ -2,11 +2,15 @@ package com.han.controller;
 
 import com.han.dto.PostListReqDto;
 import com.han.model.Post;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface PostController {
-  public ResponseEntity<List<Post>> getPostList(PostListReqDto reqDto) throws SQLException;
+  public ResponseEntity<List<Post>> getPostList(@Valid PostListReqDto reqDto) throws SQLException;
+  public ResponseEntity<Post> getPostDetail(@PathVariable @Min(1)int postId) throws SQLException;
 }
