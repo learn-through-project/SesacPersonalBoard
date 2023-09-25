f5994dcd6ba7159c99b88371fe234fede2c5d9e2
devsince2021 <dev.ryu2021@gmail.com>
2023-09-25 15:38:24 +0900

    Chore: Entity 이름 변경 (#26)

--- src/main/java/com/han/model/PostImage.java
+++ src/main/java/com/han/model/PostImage.java
@@ -3,7 +3,7 @@ package com.han.model;
 import lombok.Data;
 
 @Data
-public class PostImages {
+public class PostImage {
     private Integer id;
     private Integer postId;
     private String url;
